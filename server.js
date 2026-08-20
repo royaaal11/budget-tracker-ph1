092d16b08e81ba4f07e116efa6de7f0b041ecc16
royaaal11 <mylesyap.11@gmail.com>
2026-08-21 04:34:54 +0800

    Update

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use(express.static(__dirname));
 
 // Database setup - Use Vercel Postgres or local PostgreSQL
